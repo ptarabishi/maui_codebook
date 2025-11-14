72569530fb6f5e173161500690576febdcee73a7
Princess Tarabishi <ptarabis@uw.edu>
2025-11-13 22:17:03 -0800

    removed old get_file functions

--- src/io.py
+++ src/io.py
@@ -6,9 +6,6 @@ import logging
 import h5py
 
 LOG = logging.getLogger(__name__)
-# data_path = '/Volumes/AhmedLab/princess/data/' # for mac
-data_path = fr'/mnt/z/princess/data/' # for windows
-
 
 def load_nii(fpath: str):
     """loads volume from filepath and returns N-Dim numpy array"""
@@ -28,18 +25,6 @@ def load_pickle(fpath):
 
 def save_pickle(fpath):
     pass
-
-def get_experiment(date, id):
-    path = fr'{data_path}{date}_{id}'
-    return path
-
-def get_file(experiment_path, data_stage, extension):
-    file = glob.glob(f'{experiment_path}/{data_stage}/*{extension}')[0]
-    return file
-
-def get_dataset(path):
-    hf = h5py.File(path, 'r')
-    return hf['labels'][...]
 
 ## new functions for loading in h5 files
 def load_clusters(experiment_path):
