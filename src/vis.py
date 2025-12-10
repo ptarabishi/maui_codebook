bb9edefad6bdbc6f2cc07a940488fa5f69101a58
princesst <ptarabis@uw.edu>
2025-12-10 15:21:57 -0800

    added daily notebooks and visualization functions

--- src/vis.py
+++ src/vis.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 from matplotlib.colors import Normalize
+import numpy as np
 
 # vis features
 div_cmap = sns.color_palette('coolwarm', as_cmap=True)
@@ -23,11 +24,17 @@ def plot_raw_calcium_traces(timestamps, signal_2d, sv_toplot=30, y_shift=5):
         plt.plot(timestamps, trace, color=color)
         if idx == sv_toplot:
             break
-    return figure
 
 def plot_fictrac_and_calcium(timestamps, fictrac_param, signal_2d,  sv_toplot=30, y_shift=5):
     """
     Plots df/F calcium traces of 2d signal array aligned with behavior
+
+    Inputs:
+    timestamps: timeseries of experiment
+    fictrac_param: array of fictrac parameter over time
+    signal_2d: array of (supervoxel, df/F timeseries)
+    sv_toplot: number of supervoxel traces to plot
+    y_shift: adjust for more/less space in between traces
     """
 
     figure = plt.figure(figsize=(30,20))
@@ -47,4 +54,31 @@ def plot_fictrac_and_calcium(timestamps, fictrac_param, signal_2d,  sv_toplot=30
     plt.xlim(0, max(signal_2d)+10)
     plt.xlabel('time(s)')
 
-    return figure
+
+def plot_spatial_clusters(spatial_array, normalize_colors=False):
+    figure = plt.figure(figsize=(40, 20), constrained_layout=True)
+    gspec = figure.add_gridspec(4, 8)
+    nrows, ncols = gspec.get_geometry()
+    axs = np.array([[figure.add_subplot(gspec[i, j]) for j in range(ncols)] for i in range(nrows)])
+    norm = None
+
+    if normalize_colors == True:
+        color_range = Normalize(vmin=np.min(spatial_array), vmax=np.max(spatial_array))
+        # fig.subplots_adjust(right=0.8)
+        # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    else:
+        color_range = None
+    norm = color_range
+
+    counter = 0
+    images = []
+    for i in range(nrows):
+        for j in range(ncols):
+            images.append(axs[i, j].imshow(spatial_array[counter].T, norm=norm))
+            axs[i, j].set_title(f'Z-Slice {counter}')
+            axs[i, j].set_xticks([])
+            axs[i, j].set_yticks([])
+            counter += 1
+            if counter == spatial_array.shape[0]:
+                break
+    figure.colorbar(images[0], ax=axs)
