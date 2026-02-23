ff9472b3db74aee9d599c7b4752bdac34f8fd53f
Princess Tarabishi <ptarabis@uw.edu>
2026-02-23 11:56:56 -0800

    added init.py to src

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""Package marker for src.
+
+This file makes `src` a Python package so notebook imports like
+`from src import ttl` will work when the project root is on sys.path.
+"""
+
+__all__ = []
