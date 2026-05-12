840a6a5e5bb3bfcdbc50097d0b3f17ef1c0f131f
princesst <ptarabis@uw.edu>
2026-05-12 16:42:18 -0700

    small changes to run pip10 analysis, check before merging

--- src/moco.py
+++ src/moco.py
@@ -2,7 +2,9 @@
 
 import ants
 import numpy as np
+import time
 from src import io
+import argparse
 
 # generate mean brain based on first 300 volumes
 def generate_fixed(brain_arr, volumes:int):
@@ -20,7 +22,22 @@ def motion_correction(moving_brain, fixed_brain):
     moco_brain = np.zeros_like(moving_brain)
 
     for vol in range(n_vols):
+        start = time.time()
         moving = ants.from_numpy(moving_brain[:, :, :, vol])
         moco_brain[:, :, :, vol] = apply(fixed_brain, moving).numpy()
+        end = time.time()
+        # print(f'10 volumes took {end-start} seconds')
+        if vol % 10 == 0:
+            print(f'remaining volumes: {n_vols - vol}')
     return moco_brain
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--rawDirectory', type=str,required=True, help='Path to the moving brain image'
+)
+parser.add_argument(
+    '--saveDirectory', type=str,required=True, help='Path to the motion-corrected brain image'
+)
+
+if __name__ == '__main__':
+    motion_correction(ar)
