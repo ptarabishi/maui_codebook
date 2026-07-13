f09f1ac15d912dd303cc365221988494cbde1b99
Princess Tarabishi <ptarabis@uw.edu>
2026-07-13 13:31:59 -0700

    add voxel function

--- src/zdF.py
+++ src/zdF.py
@@ -13,8 +13,9 @@ def _zscore(x):
 def _zdff(F, win=200, smooth=False):
     """calculate zscored(df/f) based on F baseline activity"""
 
+    # F [pixel]
     # find average signal in first `win` volumes
-    Fbase = np.mean(F[:, :win], axis=-1)
+    Fbase = np.mean(F[:, :win], axis=-1) 
     dff = (F - Fbase[:, None]) / Fbase[:, None]
 
     if smooth:
@@ -37,3 +38,16 @@ def calculate_zscoredF(brain, labels_arr, n_clusters=200):
         # find zscored(df/f) and smooth over time
         ROIs[iSlice, :, :] = _zdff(mean_signal.T, win=F_WINDOW, smooth=True)
     return ROIs
+
+def calculate_zscoredF_voxels(raw_array):
+    # loop over every slice
+    x_by_y = raw_array.shape[0] * raw_array.shape[1]
+    window = raw_array.shape[2]
+
+    for iSlice in range(raw_array.shape[2]):
+        # collapse x and y
+        array = raw_array[...,iSlice, :]
+        voxel_array = raw_array.reshape(x_by_y,-1)
+
+    df = _zdff(voxel_array, win = window, smooth = True)
+    return df
